a0c85249ffc0265c2ad3631452acf7397df1f715
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-26 14:07:26 +0100

    Initial Commit

---
+++ src/main/java/org/simplon/ecf/EcfApplication.java
@@ -0,0 +1,13 @@
+package org.simplon.ecf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/simplon/ecf/EcfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simplon.ecf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
