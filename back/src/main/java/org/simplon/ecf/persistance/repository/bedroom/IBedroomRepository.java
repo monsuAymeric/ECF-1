ab5c1ea27a7c5520c3ad7b5b3331ee568806be35
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-27 15:37:20 +0100

    doc(repository): Added documentation in IBedroomRepository

--- back/src/main/java/org/simplon/ecf/persistance/repository/bedroom/IBedroomRepository.java
+++ back/src/main/java/org/simplon/ecf/persistance/repository/bedroom/IBedroomRepository.java
@@ -1,5 +1,7 @@
 package org.simplon.ecf.persistance.repository.bedroom;
 
+import java.util.List;
+
 import org.simplon.ecf.persistance.entity.Bedroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,6 +9,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IBedroomRepository extends JpaRepository<Bedroom, Long>{
+
+    /**
+     * Find bedrooms by service.
+     * 
+     * @param service the service.
+     * 
+     * @return A list of bedrooms with the given service.
+     */
     @Query(BedroomQueries.FIND_BY_SERVICE)
-    Bedroom findByService(final long id_service);
+    List<Bedroom> findByService(final long id_service);
 }
