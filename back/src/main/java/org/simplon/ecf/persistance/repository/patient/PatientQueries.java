738bb93b1ea59db2695e6555e9b166fd3dc3ca18
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-27 14:26:27 +0100

    feat(repository): Added PatientQueries

--- back/src/main/java/org/simplon/ecf/persistance/repository/patient/PatientQueries.java
+++ back/src/main/java/org/simplon/ecf/persistance/repository/patient/PatientQueries.java
@@ -1,5 +1,13 @@
 package org.simplon.ecf.persistance.repository.patient;
 
 public class PatientQueries {
-    
+
+    private PatientQueries() {
+        throw new IllegalStateException("Utility class not meant for instantation!");
+    }
+
+    public static final String FIND_BY_SERVICE = "SELECT p FROM Patient p WHERE p.service.id = :id_service";
+    public static final String FIND_BY_BED = "SELECT p FROM Patient p WHERE p.bed.id = :id_bed";
+    public static final String FIND_BY_FIRSTNAME = "SELECT p FROM Patient p WHERE p.firstname = :firstname";
+    public static final String FIND_BY_LASTNAME = "SELECT p FROM Patient p WHERE p.lastname = :lastname";
 }
