d518932b51375966f6efe5fdf444163f6aa0057c
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-27 13:51:33 +0100

    feat(repository): Added BedQueries

--- back/src/main/java/org/simplon/ecf/persistance/repository/bed/BedQueries.java
+++ back/src/main/java/org/simplon/ecf/persistance/repository/bed/BedQueries.java
@@ -2,4 +2,9 @@ package org.simplon.ecf.persistance.repository.bed;
 
 public class BedQueries {
     
+    private BedQueries() {
+        throw new IllegalStateException("Utility class not meant for instantiation!");
+    }
+
+    public static final String FIND_BY_BEDROOM = "Select c FROM Bed c WHERE c.bedroom.id = :id_bedroom";
 }
