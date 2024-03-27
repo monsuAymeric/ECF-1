c9bf40ce0c05503583cc7702c198a27d53317244
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-27 14:28:03 +0100

    feat(repository): Added ServiceQueries

--- back/src/main/java/org/simplon/ecf/persistance/repository/service/ServiceQueries.java
+++ back/src/main/java/org/simplon/ecf/persistance/repository/service/ServiceQueries.java
@@ -2,4 +2,10 @@ package org.simplon.ecf.persistance.repository.service;
 
 public class ServiceQueries {
     
+    private ServiceQueries() {
+        throw new IllegalStateException("Utility class not meant for instantiation!");
+    }
+
+    public static final String FIND_BY_NAME = "SELECT s FROM Service s WHERE s.name = :name";
+    
 }
