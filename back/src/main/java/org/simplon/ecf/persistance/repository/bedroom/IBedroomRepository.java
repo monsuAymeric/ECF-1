1bb3065043d8c6b67984db0efb4cf9bb11444d84
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-27 15:21:36 +0100

    feat(repository): Added IBedroomRepository

--- back/src/main/java/org/simplon/ecf/persistance/repository/bedroom/IBedroomRepository.java
+++ back/src/main/java/org/simplon/ecf/persistance/repository/bedroom/IBedroomRepository.java
@@ -1,5 +1,12 @@
 package org.simplon.ecf.persistance.repository.bedroom;
 
-public interface IBedroomRepository {
-    
+import org.simplon.ecf.persistance.entity.Bedroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBedroomRepository extends JpaRepository<Bedroom, Long>{
+    @Query(BedroomQueries.FIND_BY_SERVICE)
+    Bedroom findByService(final long id_service);
 }
