11a78649f0ff27ac8ffe29a2905ac72652cbc5b7
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-28 15:25:00 +0100

    feat(DTO): Added BedDto

--- back/src/main/java/org/simplon/ecf/business/dto/BedDto.java
+++ back/src/main/java/org/simplon/ecf/business/dto/BedDto.java
@@ -1,5 +1,63 @@
 package org.simplon.ecf.business.dto;
 
 public class BedDto {
+    private long id;
+    private BedroomDto bedroom;
+    private boolean available;
+
+    /**
+     * Get the bed's id.
+     * 
+     * @return the bed's id.
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Set the bed's id.
+     * 
+     * @param id the bed's id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the bed's bedroom.
+     * 
+     * @return the bed's bedroom.
+     */
+    public BedroomDto getBedroomDto() {
+        return bedroom;
+    }
+
+    /**
+     * Set the bed's bedroom.
+     * 
+     * @param bedroom the bed's bedroom to set
+     */
+    public void setBedroomDto(BedroomDto bedroom) {
+        this.bedroom = bedroom;
+    }
+
+    /**
+     * Get the bed's availability.
+     * 
+     * @return the bed's availability.
+     */
+    public boolean isAvailable() {
+        return available;
+    }
+
+    /**
+     * Set the bed's availability.
+     * 
+     * @param available the bed's availability to set
+     */
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
     
 }
