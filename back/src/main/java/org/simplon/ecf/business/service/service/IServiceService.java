5e549c934de45b75cce3d59624c684df99a2e8a4
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-29 15:26:11 +0100

    feat(service): Added IServiceService

--- back/src/main/java/org/simplon/ecf/business/service/service/IServiceService.java
+++ back/src/main/java/org/simplon/ecf/business/service/service/IServiceService.java
@@ -1,5 +1,27 @@
 package org.simplon.ecf.business.service.service;
 
+import org.simplon.ecf.business.dto.ServiceDto;
+
 public interface IServiceService {
-    
+
+    /**
+     * Saves a service in the database.
+     * 
+     * @param service the service to save.
+     */
+    void createService(final ServiceDto service);
+
+    /**
+     * Modifies the service.
+     * 
+     * @param service the service to modify.
+     */
+    void modifyService(final ServiceDto service);
+
+    /**
+     * Delete the service with the given id.
+     * 
+     * @param id the id of the product.
+     */
+    void deleteService(final long id);
 }
