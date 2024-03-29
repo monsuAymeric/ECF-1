30cf1cf46e8f1576c02acee828a8761f6209f5ba
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-29 15:55:18 +0100

    feat(controller): Added GetAllPatientController

--- back/src/main/java/org/simplon/ecf/presentation/controller/patient/GetAllPatientController.java
+++ back/src/main/java/org/simplon/ecf/presentation/controller/patient/GetAllPatientController.java
@@ -1,5 +1,29 @@
 package org.simplon.ecf.presentation.controller.patient;
 
+import java.util.List;
+
+import org.simplon.ecf.business.dto.PatientDto;
+import org.simplon.ecf.business.service.patient.IPatientService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class GetAllPatientController {
-    
+    private IPatientService patientService;
+
+    /**
+     * Injects the patient service
+     * 
+     * @param patientService the patient service
+     */
+    public GetAllPatientController(IPatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/patients")
+    public List<PatientDto> getAllPatient(){
+        return patientService.getAllPatient();
+    }
 }
