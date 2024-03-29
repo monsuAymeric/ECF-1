c4c205246f66c7378492dae3cead236d42109074
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-29 11:52:20 +0100

    feat(service) : Added BedServiceImpl

--- back/src/main/java/org/simplon/ecf/business/service/bed/BedServiceImpl.java
+++ back/src/main/java/org/simplon/ecf/business/service/bed/BedServiceImpl.java
@@ -1,5 +1,37 @@
 package org.simplon.ecf.business.service.bed;
 
-public class BedServiceImpl {
-    
+import java.util.List;
+import org.simplon.ecf.business.convert.BedConvert;
+import org.simplon.ecf.business.dto.BedDto;
+import org.simplon.ecf.persistance.repository.bed.IBedRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BedServiceImpl implements IBedService {
+    private IBedRepository bedRepository;
+
+    public BedServiceImpl(final IBedRepository bedRepository) {
+        this.bedRepository = bedRepository;
+    }
+
+    @Override
+    public void createBed(final BedDto bed) {
+        bedRepository.save(BedConvert.getInstance().dtoToEntity(bed));
+    }
+
+    @Override 
+    public List<BedDto> getAvailableBed() {
+        return BedConvert.getInstance().entityListToDtoList(bedRepository.findAll());
+    }
+
+    @Override
+    public BedDto getBedById(final long id) {
+        return BedConvert.getInstance().entityToDto(bedRepository.findById(id).orElse(null));
+    }
+
+    @Override 
+    public void deleteBed(final long id) {
+        bedRepository.deleteById(id);
+    }
 }
+
