529d6c19efbc342128ef7ecdba8f281f39bf81d3
monsuAymeric <monsu.aymeric@gmail.com>
2024-03-29 15:43:05 +0100

    feat(controller): Added CreatePatientController

--- back/src/main/java/org/simplon/ecf/presentation/controller/patient/CreatePatientController.java
+++ back/src/main/java/org/simplon/ecf/presentation/controller/patient/CreatePatientController.java
@@ -1,5 +1,28 @@
 package org.simplon.ecf.presentation.controller.patient;
 
+import org.simplon.ecf.business.dto.PatientDto;
+import org.simplon.ecf.business.service.patient.IPatientService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class CreatePatientController {
-    
+    private IPatientService patientService;
+
+    /**
+     * Injects the patient service.
+     * 
+     * @param patientService the patient service.
+     */
+    public CreatePatientController(IPatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping("/patients")
+    public void createPatient(final @RequestBody PatientDto patient) {
+        patientService.createPatient(patient);
+    }
 }
